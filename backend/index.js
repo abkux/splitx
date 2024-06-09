ae7b4c0266818d0e334dde4edf128f7ef5c8aefd
abkux <kumarjhaabhishek2002@gmail.com>
2024-06-09 21:24:19 +0530

    added initial backend code

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://localhost:5173"],
+  credentials: true,
+};
+morgan.token("referrer", function (req, res) {
+  return req.headers.referer || "-";
+});
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms - referrer: :referrer"
+  )
+);
+app.use(helmet());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cookieParser());
+
+
+app.get('/', async(req,res) => {
+    res.status(200).json({response: 'hello from splitx backend.'})
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening On Port: ${process.env.PORT}`);
+});
